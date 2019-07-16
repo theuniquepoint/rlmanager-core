b5cfc4af932f9f52acfabec1380523a9a721c44a
Alex Torregrosa <alex@theuniquepoint.com>
2019-07-16 19:22:45 +0200

    Initial commit

--- src/routes/AuthenticatorRoutes.php
+++ src/routes/AuthenticatorRoutes.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::group(['namespace' => 'Theuniquepoint\Authenticator\Http\Controllers', 'middleware' => ['Authenticator']], function(){
+Route::group(['namespace' => 'Theuniquepoint\Authenticator\Http\Controllers', 'middleware' => ['AuthenticatorRoutes']], function(){
     Route::get('login', 'AuthenticatorController@getLogin');
     Route::post('login', 'AuthenticatorController@postLogin');
 });
